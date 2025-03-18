e3e1f92ff90fa1b6279ec28b0bf8a1b31ac82634
mak025 <mak025@edu.zealand.dk>
2025-03-18 11:16:28 +0100

    Added files

---
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AgeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Task 7 Start");
+            Thread.Sleep(1000);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Please enter the birthday of the person you are assisting in the format: dd-MM-yyyy");
+            DateTime birthday = DateTime.Parse(Console.ReadLine());
+            TimeSpan difference = DateTime.Now - birthday;
+            int years = (int)Math.Floor(difference.TotalDays / 365.2425);
+            int days = (int)Math.Floor(difference.TotalDays);
+            birthday = birthday.AddYears(years);
+            difference = DateTime.Now - birthday;
+            Console.WriteLine($"The customer is {years} + years old and {days}");
+            //Console.WriteLine(years);
+
+        }
+    }
+}
